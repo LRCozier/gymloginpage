2b3e4b06a9454c87700cdcc91f307c9c9e9e0370
LRCozier <lrcozier@gmail.com>
2025-03-22 20:55:09 +0000

    built component and view

--- gymloginpage/src/components/loginform.vue
+++ gymloginpage/src/components/loginform.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
+ import {ref} from 'vue';
+
+ const email = ref('');
+ const password = ref('');
+ const emit = defineEmits(['submit']);
+
+const handleSubmit = () => {
+  emit('submit', { email: email.value, password: password.value });
+};
 
 </script>
 
 <template>
-  
+  <div class="login-form-container">
+    <form @submit.prevent="handleSubmit">
+      <div class="input">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="email" required />
+      </div>
+      <div class="input">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
---
+++ gymloginpage/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script>
+import Loginform from '../components/loginform.vue';
+
+const handleLogin = (credential) => {
+  console.log('Login attempted with:', credential);
+};
+</script>
+
+<template>
+  <Loginform @submit="handleLogin" />
+</template>
